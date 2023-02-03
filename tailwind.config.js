11b38928dd9c4644dd30717c1db4c51077b3af81
Rahul Saini <rscoder7@outlook.com>
2023-02-03 20:27:37 +1100

    Works

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'dgreen': '#70C1B3',
+      'tgreen': '#B2DBBF',
+      'lblue': '#6279B8'
+    }
+  },
+  plugins: [],
+}
