b4485cc926672be3e78687bc310c6923a9307d01
Rahul Saini <rahul.saini@education.nsw.gov.au>
2024-07-18 02:33:28 +1000

    started on scaffold

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'dgreen': '#70C1B3',
-      'tgreen': '#B2DBBF',
-      'lblue': '#6279B8'
-    }
   },
   plugins: [],
-}
+};
